599dc061663c7d38aa5559d2f257503aabdf7b0c
BatuhanNC <76656146+BatuhanNC@users.noreply.github.com>
2021-11-22 11:09:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+session_start();
+ob_start();
+?>
+<div id="footer"> <?php include '../views/include/header.php'; ?> </div>
+<div id="footer"> <?php include '../views/include/deneme.php'; ?> </div>
+<div id="footer"> <?php include 'include/footer.php'; ?> </div>
