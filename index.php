e0bedd8f6c002be8886197a883366d7f3016787a
BatuhanNC <76656146+BatuhanNC@users.noreply.github.com>
2021-11-24 16:03:18 +0300

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-ob_start();
-?>
-<div id="footer"> <?php include '../views/include/header.php'; ?> </div>
-<div id="footer"> <?php include '../views/include/deneme.php'; ?> </div>
-<div id="footer"> <?php include 'include/footer.php'; ?> </div>
